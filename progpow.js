26c15fdc5280adc374ddd38f7c369c3ebadef267
Jules de Smit <julesdesmit@gmail.com>
2020-03-29 03:28:16 +0200

    Add cache generation function

--- progpow.js
+++ progpow.js
@@ -1,9 +1,33 @@
 const FNV_PRIME = 16777619;
+const CACHE_SIZE = 1000;
+const CACHE_ROUNDS = 3;
+const KECCAK_256_SIZE = 64;
 
 var gl;
 
 function fnv_hash(v1, v2) {
 	return ((v1 * FNV_PRIME) ^ v2) % 2**32;
+}
+
+function make_cache(seed) {
+	let n = CACHE_SIZE / KECCAK_256_SIZE;
+
+	// Produce initial data set
+	let data_set = [];
+	data_set.push(keccak_512(seed));
+	for (let i = 1; i < n; i++) {
+		data_set.push(keccak_512(data_set[i-1]));
+	}
+
+	// randmemohash
+	for (let i = 0; i < CACHE_ROUNDS; i++) {
+		for (let j = 0; j < n; j++) {
+			let value = data_set[j][0] % n;
+			data_set[j] = keccak_512(data_set[(i-1+value) % value] ^ data_set[value]);
+		}
+	}
+
+	return data_set;
 }
 
 function init(threads) {
